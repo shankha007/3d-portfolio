9c8fa449000111ef0c8f6901d10e2150e7cbd641
Shankha <ribhumaster@gmail.com>
2023-04-27 00:39:35 +0530

    navbar built

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
 const App = () => {
-  return <div>3D Developer Portfolio</div>;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-center bg-no-repeat bg-cover bg-hero-pattern">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
