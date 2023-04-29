8a5ec0e23fac92c72fb59b225cf6b880b7c21477
Shankha <ribhumaster@gmail.com>
2023-04-29 16:36:56 +0530

    about section and hoc done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,6 +26,24 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas />
+
+      <div className="absolute flex items-center justify-center w-full bottom-32 xs:bottom-10">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 mb-1 rounded-full bg-secondary"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
